b6de0db2f028f425e82a5d41ce192a910f121364
Doug Koehn <dkoehn@churchcommunitybuilder.com>
2019-05-15 07:41:46 -0600

    dk - Update code coverage library

--- src/CodeCoverageProvider.php
+++ src/CodeCoverageProvider.php
@@ -4,37 +4,30 @@ namespace CCB\DspecCodeCoverage;
 
 use DSpec\Console\DSpecApplication;
 use DSpec\Container;
-use DSpec\Event\SuiteEndEvent;
 use DSpec\Event\SuiteStartEvent;
 use DSpec\Events;
 use DSpec\ServiceProviderInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use SebastianBergmann\CodeCoverage\Filter;
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Report\Html\Facade;
 
 class CodeCoverageProvider implements ServiceProviderInterface
 {
 	public function register(Container $container)
 	{
-		if (!class_exists('\PHP_CodeCoverage')) {
-			throw new \Exception('Please run composer require --dev phpunit/php-code-coverage:~2.2');
-		}
-
 		/** @var EventDispatcherInterface $dispatcher */
 		$dispatcher = $container['dispatcher'];
 
-		/** @var \PHP_CodeCoverage $coverage */
+		/** @var CodeCoverage $coverage */
 		$coverage = null;
 
 		$dispatcher->addListener(Events::SUITE_START, function(SuiteStartEvent $event) use (&$coverage, $container) {
 			$whitelistDirs = $container->offsetExists('coverage.whitelistDirs') ? $container['coverage.whitelistDirs'] : array();
-			$blacklistDirs = $container->offsetExists('coverage.blacklistDirs') ? $container['coverage.blacklistDirs'] : array();
 
 			$filter = null;
-			if ((is_array($whitelistDirs) && count($whitelistDirs) > 0) ||
-				(is_array($blacklistDirs) && count($blacklistDirs) > 0)) {
-				$filter = new \PHP_CodeCoverage_Filter();
-				foreach ($blacklistDirs as $dir) {
-					$filter->addDirectoryToBlacklist($dir);
-				}
+			if (is_array($whitelistDirs) && count($whitelistDirs) > 0) {
+				$filter = new Filter();
 				foreach ($whitelistDirs as $dir) {
 					$filter->addDirectoryToWhitelist($dir);
 				}
@@ -59,7 +52,7 @@ class CodeCoverageProvider implements ServiceProviderInterface
 
 			$container['coverage.outputDir'] = $outputDir;
 
-			$coverage = new \PHP_CodeCoverage(null, $filter);
+			$coverage = new CodeCoverage(null, $filter);
 			$coverage->start($event->getExampleGroup()->getTitle());
 		});
 
@@ -70,12 +63,10 @@ class CodeCoverageProvider implements ServiceProviderInterface
 
 			$reportWriterType = $container->offsetExists('coverage.reportWriter') ? $container['coverage.reportWriter'] : 'html';
 
-			switch ($reportWriterType) {
-				case 'html':
-				default:
-					$writer = new \PHP_CodeCoverage_Report_HTML();
-					break;
+			if ($reportWriterType === 'html') {
+				$writer = new Facade();
 			}
+
 			$writer->process($coverage, $outputDir);
 		});
 	}
